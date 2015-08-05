6e329320574678341a7efe9ced0d805f6f7ed2fd
Jiri Uher <jiri.uher@nextsoft.cz>
2015-08-05 12:31:14 +0200

    Finally know how to fix read-only table updates.

--- Mvc5Awesome/Models/TpiiEntities.Designer.cs
+++ Mvc5Awesome/Models/TpiiEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\_download\Mvc5Awesome\Mvc5Awesome\Models\TpiiEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Dropbox\Dokumenty\Visual Studio 2013\Projects\Mvc5Awesome\Mvc5Awesome\Models\TpiiEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
